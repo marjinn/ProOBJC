38775b942b2ad2fcf8a783e07573088725ae6cdb
mar Jinn <marjinn@mars-Mac.local>
2014-08-14 02:24:28 +0530

    Initial Commit

---
+++ PRO-ObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PRO-ObjectiveC
+//
+//  Created by mar Jinn on 8/14/14.
+//  Copyright (c) 2014 mar Jinn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
